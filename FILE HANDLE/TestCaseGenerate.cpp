a1a3fcb09440bc07d94149057f1e5cc167a10713
Dhruv_gheewala <52659909+DhruvGheewala@users.noreply.github.com>
2020-01-27 19:04:37 +0530

    Update TestCaseGenerate.cpp

--- FILE HANDLE/TestCaseGenerate.cpp
+++ FILE HANDLE/TestCaseGenerate.cpp
@@ -5,9 +5,10 @@ int main()
 {
 
     #ifndef ONLINE_JUDGE
-    	freopen("TESTCASE.txt","r",stdin);
-    	freopen("INPUT.txt","w",stdout);
+	freopen("TESTCASE.txt","r",stdin);
+    freopen("INPUT.txt","w",stdout);
     #endif
+
 
 	/*This code will be changed according to generation in test-case.*/
     /*srand(time(0));

31f4c2499e4fba5fef89fdc67d6e5a9b46683d47
Dhruv_gheewala <52659909+DhruvGheewala@users.noreply.github.com>
2020-03-20 21:00:22 +0530

    Update quote.py

--- Sublime-Snippets/quote.py
+++ Sublime-Snippets/quote.py
@@ -7,6 +7,12 @@ from datetime import datetime
 
 quotes = [
 '''/***
+ ** "Never Forget What you are,the rest of the world will not.Wear it like armour and it can never be used to hurt you"
+ **     -- Tyrion Lannister
+***/
+''',
+
+'''/***
  ** "Learn to forgive the ones who deserve it…and the ones who don’t."
  **
 ***/
@@ -62,79 +68,79 @@ quotes = [
 
 '''/***
  ** " Dream is not that which you see while sleeping it is something that does not let you sleep "
- ** 	-- A.P.J Abdul Kalam
+ **     -- A.P.J Abdul Kalam
 ***/
 ''',
 
 '''/***
  ** " All that we are is the result of what we have thought. "
- ** 	-- Buddha
+ **     -- Buddha
 ***/
 ''',
 
 '''/***
  ** " Love the life you live. Live the life you love. "
- ** 	-- Bob Marley
+ **     -- Bob Marley
 ***/
 ''',
 
 '''/***
  ** " I have not failed. I've just found 10,000 ways that won't work. "
- ** 	-- Thomas Edison
+ **     -- Thomas Edison
 ***/
 ''',
 
 '''/***
  ** " If you can't explain it simply, you don't understand it well enough. "
- ** 	-- Albert Einstein
+ **     -- Albert Einstein
 ***/
 ''',
 
 '''/***
  ** " Success is most often achieved by those who don't know that failure is inevitable. "
- ** 	-- Coco Chanel
+ **     -- Coco Chanel
 ***/
 ''',
 
 '''/***
  ** " Be yourself. Everyone else is already taken." 
- ** 	-- Oscar Wilde
+ **     -- Oscar Wilde
 ***/
 ''',
 
 '''/***
  ** " Your limitation it's only your imagination. "
- ** 	-- Pablo Picasso
+ **     -- Pablo Picasso
 ***/
 ''',
 
 '''/***
  ** " Wake up with determination. Go to bed with satisfaction. "
- ** 	-- George Lorimer
+ **     -- George Lorimer
 ***/
 ''',
 
 '''/***
  ** " Experience is the name everyone gives to their mistakes. "
- ** 	-- Oscar Wilde
+ **     -- Oscar Wilde
 ***/
 ''',
 
 '''/***
  ** " Knowledge is power. "
- ** 	-- Francis Bacon
+ **     -- Francis Bacon
 ***/
 ''',
 
 '''/***
  ** " In order to be irreplaceable, one must always be different "
- ** 	-- Coco Chanel
+ **     -- Coco Chanel
 ***/
 ''',
 
 '''/***
  ** " Simplicity is the soul of efficiency. "
- ** 	-- Austin Freeman
+ **     -- Austin Freeman
 ***/
 ''',
 

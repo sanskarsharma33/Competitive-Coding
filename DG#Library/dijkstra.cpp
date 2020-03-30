d09386a4da205811c939f296ab2dab4b5a56f361
Dhruv_gheewala <52659909+DhruvGheewala@users.noreply.github.com>
2020-03-31 02:36:18 +0530

    Create dijkstra.cpp

---
+++ DG#Library/dijkstra.cpp
@@ -0,0 +1,49 @@
+struct node
+{
+    int p;
+    ll cost;
+};
+string to_string(const node &a)
+{
+    #ifdef DEBUG
+        return to_string({p, cost});
+    #endif
+        return "";
+}
+
+void add_edge(vector<vector<pair<int, ll>>> &a, int u, int v, ll w, bool dir = 0)
+{
+    a[u].push_back({v, w});
+    if(!dir)
+        a[v].push_back({u, w});
+}
+
+vector<vector<pair<int, ll>>> adj;
+set<pii> s;
+vector<node> sssp;
+
+void sssp_dijkstra(int root = 1)
+{
+    int n = adj.size() - 1;
+    sssp.assign(n + 1, {-1, INT_MAX});
+
+    sssp[root] = {root, 0};
+    s.insert({0, root});
+    while(!s.empty())
+    {
+        auto p = s.begin();
+        int u = p->ss;
+        s.erase(s.begin());
+
+        for(auto v : adj[u])
+        {
+            int dist = sssp[u].cost + v.ss;
+            if(dist < sssp[v.ff].cost)
+            {
+                s.erase({sssp[v.ff].cost, v.ff});
+                sssp[v.ff] = {u, dist};
+                s.insert({dist, v.ff});
+            }
+        }
+    }
+}
